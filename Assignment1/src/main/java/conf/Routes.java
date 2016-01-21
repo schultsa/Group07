ba7e5cee0bcce9e67e0a3ce4ac8edb778b001399
charan96 <charan.sudarsanam@gmail.com>
2016-01-20 21:18:54 -0800

    Made changes to Routes for Index Added card model and test class for it

--- Assignment1/src/main/java/conf/Routes.java
+++ Assignment1/src/main/java/conf/Routes.java
@@ -27,7 +27,7 @@ public class Routes implements ApplicationRoutes {
     @Override
     public void init(Router router) {  
         
-        router.GET().route("/").with(ApplicationController.class, "index");
+        router.GET().route("/index4").with(ApplicationController.class, "index");
 //        router.GET().route("/hello_world.json").with(ApplicationController.class, "helloWorldJson");
         router.GET().route("/AcesUp").with(ApplicationController.class, "acesUp");
 
