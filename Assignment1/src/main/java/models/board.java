7b5a14dff8cbd675e03ebee630010863521e2b20
schultsa <schultsa@onid.oregonstate.edu>
2016-01-21 19:34:05 -0800

    Working on Board code

---
+++ Assignment1/src/main/java/models/board.java
@@ -0,0 +1,16 @@
+package models;
+
+/**
+ * Created by Sam on 1/21/2016.
+ */
+import ninja.Result;
+import ninja.Results;
+import java.util.List;
+import java.util.*;
+
+public class board {
+    public void board(){
+        //List<Stack<card>> board = new ArrayList<Stack<card>>();
+        Vector<Vector<card>> board = new Vector<Vector<card>>();
+    }
+}
--- Assignment1/src/test/java/models/appModelTest.java
+++ Assignment1/src/test/java/models/appModelTest.java
@@ -1,12 +1,14 @@
 package models;
 
 import org.junit.Test;
-
+import java.util.*;
 import ninja.NinjaDocTester;
 import org.doctester.testbrowser.Request;
 import org.doctester.testbrowser.Response;
 import org.hamcrest.CoreMatchers;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 /**
  * Created by ramcharan on 1/20/16.
@@ -17,6 +19,16 @@ public class appModelTest extends NinjaDocTester{
     public void testObjCreation()
     {
         card newCard=new card();
+        assertThat(newCard, instanceOf(card.class));
+
+    }
+
+    @Test
+    public void testBoardModel(){
+        board newBoard = new board();
+        assertThat(newBoard, instanceOf(board.class));
+        newBoard.
+
     }
 
 }
