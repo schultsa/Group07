ff715c3cdbe4a43bd61433f8a5abed2210f783e6
Dennis Lee <leed@oregonstate.edu>
2016-01-21 19:43:42 -0800

    Testing commit

--- Assignment1/src/main/java/models/board.java
+++ Assignment1/src/main/java/models/board.java
@@ -3,5 +3,10 @@ package models;
 /**
  * Created by Lee on 1/21/2016.
  */
+import java.util.*;
+import com.google.inject.Singleton;
+
+@Singleton
 public class board {
+
 }
--- Assignment1/src/test/java/models/appModelTest.java
+++ Assignment1/src/test/java/models/appModelTest.java
@@ -3,11 +3,11 @@ package models;
 import org.junit.Test;
 
 import ninja.NinjaDocTester;
-import org.doctester.testbrowser.Request;
-import org.doctester.testbrowser.Response;
-import org.hamcrest.CoreMatchers;
-import static org.hamcrest.CoreMatchers.containsString;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import java.util.*;
 /**
  * Created by ramcharan on 1/20/16.
  */
@@ -17,6 +17,14 @@ public class appModelTest extends NinjaDocTester{
     public void testObjCreation()
     {
         card newCard=new card();
+        assertThat(newCard,instanceOf(card.class));
+    }
+
+    @Test
+    public void testBoardCreation(){
+        board newBoard = new board();
+        assertThat(newBoard,instanceOf(board.class));
+
     }
 
 }
