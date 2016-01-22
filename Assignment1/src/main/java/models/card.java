dbcef5c8d8b82aff4a4097159c40e223468a151e
charan96 <charan.sudarsanam@gmail.com>
2016-01-21 19:28:08 -0800

    Finished implementing the card class

--- Assignment1/src/main/java/models/card.java
+++ Assignment1/src/main/java/models/card.java
@@ -3,6 +3,7 @@ package models;
 /**
  * Created by ramcharan on 1/20/16.
  */
+//import com.sun.org.apache.xpath.internal.operations.String;
 import ninja.Result;
 import ninja.Results;
 
@@ -12,6 +13,19 @@ import com.google.inject.Singleton;
 @Singleton
 public class card {
 
-        String suit="";
-        String value="";
+    String suit;
+    String value;
+
+    public card()
+    {
+        this.suit="";
+        this.value="";
+    }
+
+    public card(String suit, String value)
+    {
+        this.suit=suit;
+        this.value=value;
+    }
+
 }
