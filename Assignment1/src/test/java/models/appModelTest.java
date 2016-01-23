49c56332045d435b65cf12ee4c1d33c6c83952bb
Dennis Lee <leed@oregonstate.edu>
2016-01-22 16:13:15 -0800

    Decomposed first function

--- Assignment1/src/test/java/models/appModelTest.java
+++ Assignment1/src/test/java/models/appModelTest.java
@@ -6,10 +6,12 @@ import ninja.NinjaDocTester;
 import org.doctester.testbrowser.Request;
 import org.doctester.testbrowser.Response;
 import org.hamcrest.CoreMatchers;
+import org.mockito.internal.matchers.InstanceOf;
+
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
+
 import java.util.*;
 
 /**
@@ -43,9 +45,26 @@ public class appModelTest extends NinjaDocTester{
     @Test
     public void testBoard(){
         board newBoard = new board();
-        card newCard = new card();
+        assertThat(newBoard,instanceOf(board.class) );
+    }
+
+    @Test
+    public void testBoardInit(){
+        board newBoard = new board();
+        //card newCard = new card("Diamond", "Jack");
+        newBoard.init();
+        for(int i =0;i<4;i++){
+            assertTrue(newBoard.boardArray[i].empty());
+        }
+
+    }
+
+    @Test
+    public void testBoardAdd(){
+        board newBoard = new board();
+        card newCard = new card("Diamond", "Jack");
         newBoard.init();
         newBoard.addStack(0,newCard);
-        newBoard.topStack(0);
+        assertTrue(!(newBoard.boardArray[0].empty()));
     }
 }
